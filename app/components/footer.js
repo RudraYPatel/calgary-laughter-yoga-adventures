af26a0b8590e070c93caf99d89889a35aada2b11
Smit Harshadkumar Vyas <smitvyas83@gmail.com>
2024-07-23 15:29:01 -0600

    admin login page commit

---
+++ app/components/footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-white py-4 border-t border-gray-300">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+          <img src="/logo (2).png" alt="logo" className="h-28 rounded-3xl"/>
+            <div className="ml-24 text-black">
+              <div className="font-bold mb-2">Explore</div>
+              <ul>
+                <li><a href="#" className="hover:underline">Our Club</a></li>
+                <li><a href="#" className="hover:underline">Donations</a></li>
+              </ul>
+            </div>
+            <div className="ml-44 text-black">
+              <div className="font-bold mb-2">Service</div>
+              <ul>
+                <li><a href="#" className="hover:underline">FAQs</a></li>
+                <li><a href="#" className="hover:underline">Contact us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="text-black mr-32">
+            <div className="font-bold mb-2">Contact</div>
+            <ul>
+              <li className="flex items-center">
+              
+                <span className="ml-2">405-995-6546</span>
+              </li>
+              <li className="flex items-center">
+        
+                <span className="ml-2">laughteradventurescanada@gmail.com</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="mt-4  text-right text-sm text-black">
+          <div>© 2023 by YYC Laughter Yoga And Adventures.</div>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ app/components/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    
+    <div className="flex bg-white  p-2">
+        <img src="/logo (2).png" alt="logo" className="h-28 rounded-3xl"/>
+        {/* <h1 className="text-2xl mt-7 ml-5 font-bold"></h1> */}
+        <div className="flex flex-row ml-auto">
+            <Link className="text-xl mt-9 ml-28 font-semibold hover:underline" href="../">Home</Link>
+            <Link className="text-xl mt-9 ml-28 font-semibold hover:underline" href="./upcoming-events">Upcoming Events</Link>
+            <Link className="text-xl mt-9 ml-28 font-semibold hover:underline" href="">Training</Link>
+            <Link className="text-xl mt-9 ml-28 font-semibold hover:underline" href="">Speakers</Link>
+            <Link className="text-xl mt-9 ml-28 mr-10font-semibold hover:underline" href="">About</Link>
+
+        </div>  
+  </div>
+  
+  )
+};
