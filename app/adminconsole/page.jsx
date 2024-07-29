53adee2ced9f8ae6ddfa2f64e2cd7025c5ad5746
Smit Harshadkumar Vyas <smitvyas83@gmail.com>
2024-07-29 15:52:49 -0600

    PAGE JSX COMMIT

--- app/adminconsole/page.jsx
+++ app/adminconsole/page.jsx
@@ -14,47 +14,48 @@ export default function AdminConsole() {
 
   return (
     <main>
-      {user? (<div>
-        <Header />
-        <main
-        className="flex-grow flex items-center justify-center bg-cover bg-center p-6 "
-        style={{ backgroundImage: "url('/bgimage.webp')" }}
-      >
-        <div className="bg-white bg-opacity-75 p-10 rounded-xl shadow-md text-center max-w-lg mx-auto">
-          <img
-            src="/avatar.png"
-            alt="User Avatar"
-            className="h-24 w-24 mx-auto rounded-full mb-8"
-          />
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
-              View as user
-            </button>
-            <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
-              Manage Dashboard
-            </button>
-            <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
-              Update Schedule
-            </button>
-            <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
-              Manage User Info
-            </button>
-          </div>
-          <button
-            onClick={handleSignOut}
-            className="mt-8 bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800"
-          >
-            Log Out
-          </button>
+      {user ? (
+        <div>
+          <Header />
+          <main className="flex-grow flex items-center justify-center relative p-6">
+            <div
+              className="absolute inset-0 bg-cover bg-center"
+              style={{ backgroundImage: "url('/bgimage.webp')" }}
+            ></div>
+            <div className="absolute inset-0 bg-white opacity-50"></div>
+            <div className="relative z-10 bg-white bg-opacity-75 p-10 rounded-xl shadow-md text-center max-w-lg mx-auto">
+              <img
+                src="/avatar.png"
+                alt="User Avatar"
+                className="h-24 w-24 mx-auto rounded-full mb-8"
+              />
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
+                  View as user
+                </button>
+                <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
+                  Manage Dashboard
+                </button>
+                <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
+                  Update Schedule
+                </button>
+                <button className="bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800">
+                  Manage User Info
+                </button>
+              </div>
+              <button
+                onClick={handleSignOut}
+                className="mt-8 bg-black text-white py-3 px-6 rounded-md hover:bg-gray-800"
+              >
+                Log Out
+              </button>
+            </div>
+          </main>
+          <Footer />
         </div>
-      </main>
-      <Footer />
-      </div>) : 
-      redirect('/')
-      }
-      
-     
-
+      ) : (
+        redirect('/')
+      )}
     </main>
   );
 }
